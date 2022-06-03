adee8531867dd014b44039045bb3f1f1a6e7c888
LaM0uette <lam0uette.dev@gmail.com>
2022-06-03 15:52:22 +0200

    changement d'emplacement d'export bug

--- ConcatFile.go
+++ ConcatFile.go
@@ -14,9 +14,12 @@ func main() {
 	pkg.DrawStart()
 	pkg.DrawSep("BUILD")
 
+	pt := "T:\\RIP FTTH\\GEOMAP\\5_EXPORTS\\RIP40\\NRO7_PM3_NISY\\DOE_1_100%\\V12\\DLG"
+	//pt := SrcFile: pkg.GetCurrentDir(),
+
 	d := pkg.Data{
-		SrcFile: "T:\\RIP FTTH\\GEOMAP\\5_EXPORTS\\RIP40\\NRO7_PM3_NISY\\DOE_1_100%\\V12\\DLG",
-		DstFile: path.Join(pkg.GetCurrentDir(), "__Concat__"),
+		SrcFile: pt,
+		DstFile: path.Join(pt, "__Concat__"),
 	}
 
 	d.CreateExcelFile()
@@ -29,5 +32,3 @@ func main() {
 	pkg.DrawSep(" FIN ")
 	pkg.DrawEnd()
 }
-
-//SrcFile: pkg.GetCurrentDir(),
