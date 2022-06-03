8f7f4a7cd59e40677432840200b0e1eee1383892
LaM0uette <lam0uette.dev@gmail.com>
2022-06-03 15:47:18 +0200

    recupere le nom du dlg plus copie lmes fichiers correction bug

--- pkg/ConcatCSVGrace.go
+++ pkg/ConcatCSVGrace.go
@@ -19,12 +19,12 @@ func (d *Data) ConcatCSVGrace() {
 
 	DrawParam("COPIE DES CSV")
 	dlgPath := path.Join(d.SrcFile, d.GetFolderDLG())
-	CopyFile(path.Join(dlgPath, "t_cable.csv"), d.DstFile)
-	CopyFile(path.Join(dlgPath, "t_cassette.csv"), d.DstFile)
-	CopyFile(path.Join(dlgPath, "t_ebp.csv"), d.DstFile)
-	CopyFile(path.Join(dlgPath, "t_fibre.csv"), d.DstFile)
-	CopyFile(path.Join(dlgPath, "t_position.csv"), d.DstFile)
-	CopyFile(path.Join(dlgPath, "t_tiroir.csv"), d.DstFile)
+	CopyFile(path.Join(dlgPath, "t_cable.csv"), path.Join(d.DstFile, "t_cable.csv"))
+	CopyFile(path.Join(dlgPath, "t_cassette.csv"), path.Join(d.DstFile, "t_cassette.csv"))
+	CopyFile(path.Join(dlgPath, "t_ebp.csv"), path.Join(d.DstFile, "t_ebp.csv"))
+	CopyFile(path.Join(dlgPath, "t_fibre.csv"), path.Join(d.DstFile, "t_fibre.csv"))
+	CopyFile(path.Join(dlgPath, "t_position.csv"), path.Join(d.DstFile, "t_position.csv"))
+	CopyFile(path.Join(dlgPath, "t_tiroir.csv"), path.Join(d.DstFile, "t_tiroir.csv"))
 }
 
 func (d *Data) GetFolderDLG() string {
