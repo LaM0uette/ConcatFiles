53720e21f62ec944365762c8a8b95e82d1736f72
LaM0uette <lam0uette.dev@gmail.com>
2022-06-03 14:14:18 +0200

    ajout du switch case de compilation avec flag

--- ConcatFile.go
+++ ConcatFile.go
@@ -1,13 +1,24 @@
 package main
 
-import "ConcatFile/pkg"
+import (
+	"ConcatFile/pkg"
+	"flag"
+)
 
 func main() {
+
+	FlgMode := flag.String("m", "jointureGrace", "Mode de compilation")
+	flag.Parse()
 
 	pkg.DrawStart()
 
 	pkg.DrawSep("BUILD")
 	pkg.CreateExcelFile()
+
+	switch *FlgMode {
+	case "jointureGrace":
+
+	}
 
 	pkg.DrawSep(" FIN ")
 	pkg.DrawEnd()
