469743f9c2ef75afd080b12d1de7ba0bb09638fa
LaM0uette <lam0uette.dev@gmail.com>
2022-06-07 11:37:04 +0200

    up

--- pkg/ConcatCSVGrace.go
+++ pkg/ConcatCSVGrace.go
@@ -3,7 +3,10 @@ package pkg
 import (
 	"ConcatFiles/loger"
 	"bufio"
+	"database/sql"
 	"encoding/csv"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
 	"os"
 	"path"
 	"path/filepath"
@@ -21,6 +24,20 @@ func (d *Data) ConcatCSVGrace() {
 	DrawParam("NOMBRE DE POSTIONS:", strconv.Itoa(d.CountPositions()))
 
 	DrawSep("LANCEMENT DE LA COMPILATION")
+
+	database, _ := sql.Open("sqlite3", "./foo.db")
+	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY, firstname TEXT, lastname TEXT)")
+	statement.Exec()
+	statement, _ = database.Prepare("INSERT INTO people (firstname, lastname) VALUES (?, ?)")
+	statement.Exec("Nic", "Raboy")
+	rows, _ := database.Query("SELECT id, firstname, lastname FROM people")
+	var id int
+	var firstname string
+	var lastname string
+	for rows.Next() {
+		rows.Scan(&id, &firstname, &lastname)
+		fmt.Println(strconv.Itoa(id) + ": " + firstname + " " + lastname)
+	}
 }
 
 func (d *Data) GetFolderDLG() string {
