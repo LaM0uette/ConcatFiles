6865f9ac11da9c33a7bd9a023ba6b61320d9a566
LaM0uette <lam0uette.dev@gmail.com>
2022-06-09 16:44:38 +0200

    nettoyage

--- pkg/ConcatCSVGrace.go
+++ pkg/ConcatCSVGrace.go
@@ -76,32 +76,14 @@ func (d *Data) ConcatCSVGrace() {
 
 	DrawSep("COMPILATION")
 	d.runConcat(path.Join(d.DstFile, NameTPosition))
-	setFormatingWb()
-
-	//err := Wba.AddVBAProject("T:\\- 4 Suivi Appuis\\26_MACROS\\GO\\ConcatFiles\\Docs\\MJG.bin")
-	//if err != nil {
-	//	loger.Error("Erreur pendant l'ajout du module VBA'", err)
-	//}
-
-	_ = Wba.SetCellValue("MACRO", "A3", d.DstFile)
-
-	_ = Wba.SetAppProps(&excelize.AppProperties{
-		Application:       "Microsoft Excel",
-		ScaleCrop:         true,
-		DocSecurity:       2,
-		Company:           "LaM0uette",
-		LinksUpToDate:     true,
-		HyperlinksChanged: true,
-		AppVersion:        "16.0000",
-	})
-
-	Wba.SetActiveSheet(1)
 
 	if err := Wba.SaveAs(d.XlFile); err != nil {
 		loger.Error("Erreur pendant la sauvegarde du fichier Excel:", err)
 	}
 }
 
+//...
+// Functions
 func (d *Data) copyCSV() {
 	dlgPath := path.Join(d.SrcFile, d.getFolderDLG())
 
@@ -165,130 +147,46 @@ func (d *Data) appendDatasInStructs() {
 	appendEbp(path.Join(d.DstFile, NameTEbp))
 	appendTirroir(path.Join(d.DstFile, NameTTiroir))
 }
-func appendFibre(file string) {
-	Csv := ReadCSV(file)
-
-	for _, val := range Csv {
-		Item := Fibre{
-			FoCode:    val[0],
-			FoNumTube: val[4],
-			FoColor:   val[8],
-			FoCbCode:  val[2],
-		}
-		TFibre = append(TFibre, Item)
-	}
-}
-func appendCable(file string) {
-	Csv := ReadCSV(file)
-
-	for _, val := range Csv {
-		Item := Cable{
-			CbCode: val[0],
-			CbEti:  val[2],
-		}
-		TCable = append(TCable, Item)
-	}
-}
-func appendCassette(file string) {
-	Csv := ReadCSV(file)
-
-	for _, val := range Csv {
-		Item := Cassette{
-			CsCode:   val[0],
-			CsNum:    val[3],
-			CsBpCode: val[2],
-		}
-		TCassette = append(TCassette, Item)
-	}
-}
-func appendEbp(file string) {
-	Csv := ReadCSV(file)
-
-	for _, val := range Csv {
-		Item := Ebp{
-			BpCode: val[0],
-			BpEti:  val[1],
-		}
-		TEbp = append(TEbp, Item)
-	}
-}
-func appendTirroir(file string) {
-	Csv := ReadCSV(file)
-
-	for _, val := range Csv {
-		Item := Tirroir{
-			TiCode: val[0],
-			TiEti:  val[2],
-		}
-		TTirroir = append(TTirroir, Item)
-	}
-}
 
 func (d *Data) runConcat(file string) {
 	CsvData := ReadCSV(file)
-
+	Sht := "Sheet1"
 	NbrTot := 0
 
 	for r, val := range CsvData {
-		//fo1 := getDataFibre(val[2])   //ps1 et cb1
-		//fo2 := getDataFibre(val[3])   //ps2 et cb2
-		//cs := getDataCassette(val[4]) //psCsCode
-		//bp := getDataEbp(cs[1])       //csCode
-		//ti := getDataTirroir(val[5])  //tiCode
+		r++
 
-		//PsCode, _ := Sht.Cell(r, 0)
-		//PsNum, _ := Sht.Cell(r, 1)
-		//Ps1, _ := Sht.Cell(r, 2)
-		//FoNumTube1, _ := Sht.Cell(r, 3)
-		//FoColor1, _ := Sht.Cell(r, 4)
-		//CbEti1, _ := Sht.Cell(r, 5)
-		//Ps2, _ := Sht.Cell(r, 6)
-		//FoNumTube2, _ := Sht.Cell(r, 7)
-		//FoColor2, _ := Sht.Cell(r, 8)
-		//CbEti2, _ := Sht.Cell(r, 9)
-		//PsCsCode, _ := Sht.Cell(r, 10)
-		//CsNum, _ := Sht.Cell(r, 11)
-		//BpEti, _ := Sht.Cell(r, 12)
-		//PsTiCode, _ := Sht.Cell(r, 13)
-		//TiEti, _ := Sht.Cell(r, 14)
-		//PsType, _ := Sht.Cell(r, 15)
-		//PsFunc, _ := Sht.Cell(r, 16)
-		//PsState, _ := Sht.Cell(r, 17)
-		//PsPreaff, _ := Sht.Cell(r, 18)
-		//PsComment, _ := Sht.Cell(r, 19)
-		//PsCreaDate, _ := Sht.Cell(r, 20)
-		//PsMajDate, _ := Sht.Cell(r, 21)
-		//PsMajSrc, _ := Sht.Cell(r, 22)
-		//PsAbdDate, _ := Sht.Cell(r, 23)
-		//PsAbdSrc, _ := Sht.Cell(r, 24)
+		fo1 := getDataFibre(val[2])   //ps1 et cb1
+		fo2 := getDataFibre(val[3])   //ps2 et cb2
+		cs := getDataCassette(val[4]) //psCsCode
+		bp := getDataEbp(cs[1])       //csCode
+		ti := getDataTirroir(val[5])  //tiCode
 
-		_ = Wba.SetCellValue("Sheet1", fmt.Sprintf("A%v", r+1), val[0])
-
-		//PsCode.Value = val[0]
-		//PsNum.Value = val[1]
-		//Ps1.Value = val[2]
-		//FoNumTube1.Value = fo1[0]
-		//FoColor1.Value = fo1[1]
-		//CbEti1.Value = fo1[2]
-		//Ps2.Value = val[3]
-		//FoNumTube2.Value = fo2[0]
-		//FoColor2.Value = fo2[1]
-		//CbEti2.Value = fo2[2]
-		//PsCsCode.Value = val[4]
-		//CsNum.Value = cs[0]
-		//BpEti.Value = bp
-		//PsTiCode.Value = val[5]
-		//TiEti.Value = ti
-		//PsType.Value = val[6]
-		//PsFunc.Value = val[7]
-		//PsState.Value = val[8]
-		//PsPreaff.Value = val[9]
-		//PsComment.Value = val[10]
-		//PsCreaDate.Value = val[11]
-		//PsMajDate.Value = val[12]
-		//PsMajSrc.Value = val[13]
-		//PsAbdDate.Value = val[14]
-		//PsAbdSrc.Value = val[15]
+		_ = Wba.SetCellValue(Sht, fmt.Sprintf("A%v", r), val[0])  //PsCode
+		_ = Wba.SetCellValue(Sht, fmt.Sprintf("B%v", r), val[1])  //PsNum
+		_ = Wba.SetCellValue(Sht, fmt.Sprintf("C%v", r), val[2])  //Ps1
+		_ = Wba.SetCellValue(Sht, fmt.Sprintf("D%v", r), fo1[0])  //FoNumTube1
+		_ = Wba.SetCellValue(Sht, fmt.Sprintf("E%v", r), fo1[1])  //FoColor1
+		_ = Wba.SetCellValue(Sht, fmt.Sprintf("F%v", r), fo1[2])  //CbEti1
+		_ = Wba.SetCellValue(Sht, fmt.Sprintf("G%v", r), val[3])  //Ps2
+		_ = Wba.SetCellValue(Sht, fmt.Sprintf("H%v", r), fo2[0])  //FoNumTube2
+		_ = Wba.SetCellValue(Sht, fmt.Sprintf("I%v", r), fo2[1])  //FoColor2
+		_ = Wba.SetCellValue(Sht, fmt.Sprintf("J%v", r), fo2[2])  //CbEti2
+		_ = Wba.SetCellValue(Sht, fmt.Sprintf("K%v", r), val[4])  //PsCsCode
+		_ = Wba.SetCellValue(Sht, fmt.Sprintf("L%v", r), cs[0])   //CsNum
+		_ = Wba.SetCellValue(Sht, fmt.Sprintf("M%v", r), bp)      //BpEti
+		_ = Wba.SetCellValue(Sht, fmt.Sprintf("N%v", r), val[5])  //PsTiCode
+		_ = Wba.SetCellValue(Sht, fmt.Sprintf("O%v", r), ti)      //TiEti
+		_ = Wba.SetCellValue(Sht, fmt.Sprintf("P%v", r), val[6])  //PsType
+		_ = Wba.SetCellValue(Sht, fmt.Sprintf("Q%v", r), val[7])  //PsFunc
+		_ = Wba.SetCellValue(Sht, fmt.Sprintf("R%v", r), val[8])  //PsState
+		_ = Wba.SetCellValue(Sht, fmt.Sprintf("S%v", r), val[9])  //PsPreaff
+		_ = Wba.SetCellValue(Sht, fmt.Sprintf("T%v", r), val[10]) //PsComment
+		_ = Wba.SetCellValue(Sht, fmt.Sprintf("U%v", r), val[11]) //PsCreaDate
+		_ = Wba.SetCellValue(Sht, fmt.Sprintf("V%v", r), val[12]) //PsMajDate
+		_ = Wba.SetCellValue(Sht, fmt.Sprintf("W%v", r), val[13]) //PsMajSrc
+		_ = Wba.SetCellValue(Sht, fmt.Sprintf("X%v", r), val[14]) //PsAbdDate
+		_ = Wba.SetCellValue(Sht, fmt.Sprintf("Y%v", r), val[15]) //PsAbdSrc
 
 		if len(TPositionErr) > 0 {
 			if checkPosErr(val[0]) {
@@ -302,10 +200,78 @@ func (d *Data) runConcat(file string) {
 		loger.Void(fmt.Sprintf("%v/%v", NbrTot, d.NbrPos))
 	}
 
+	d.setFormatingWb()
 	loger.Ok(fmt.Sprintf("%v positions concaténées", NbrTot))
 }
 
-func setFormatingWb() {
+//...
+// Appends
+func appendFibre(file string) {
+	Csv := ReadCSV(file)
+
+	for _, val := range Csv {
+		Item := Fibre{
+			FoCode:    val[0],
+			FoNumTube: val[4],
+			FoColor:   val[8],
+			FoCbCode:  val[2],
+		}
+		TFibre = append(TFibre, Item)
+	}
+}
+
+func appendCable(file string) {
+	Csv := ReadCSV(file)
+
+	for _, val := range Csv {
+		Item := Cable{
+			CbCode: val[0],
+			CbEti:  val[2],
+		}
+		TCable = append(TCable, Item)
+	}
+}
+
+func appendCassette(file string) {
+	Csv := ReadCSV(file)
+
+	for _, val := range Csv {
+		Item := Cassette{
+			CsCode:   val[0],
+			CsNum:    val[3],
+			CsBpCode: val[2],
+		}
+		TCassette = append(TCassette, Item)
+	}
+}
+
+func appendEbp(file string) {
+	Csv := ReadCSV(file)
+
+	for _, val := range Csv {
+		Item := Ebp{
+			BpCode: val[0],
+			BpEti:  val[1],
+		}
+		TEbp = append(TEbp, Item)
+	}
+}
+
+func appendTirroir(file string) {
+	Csv := ReadCSV(file)
+
+	for _, val := range Csv {
+		Item := Tirroir{
+			TiCode: val[0],
+			TiEti:  val[2],
+		}
+		TTirroir = append(TTirroir, Item)
+	}
+}
+
+//...
+// Actions
+func (d *Data) setFormatingWb() {
 	headers := map[string]string{
 		"A1": "ps_code",
 		"B1": "ps_numero",
@@ -349,6 +315,9 @@ func setFormatingWb() {
 	_ = Wba.SetColWidth("Sheet1", "L", "L", 10)
 	_ = Wba.SetColWidth("Sheet1", "P", "R", 8)
 
+	_ = Wba.SetCellValue("MACRO", "A3", d.DstFile)
+	Wba.SetActiveSheet(1)
+
 	//Sht.SetColWidth(1, 25, 18)
 	//Sht.SetColWidth(2, 2, 10)
 	//Sht.SetColWidth(4, 5, 10)
@@ -364,48 +333,6 @@ func checkPosErr(ps string) bool {
 		}
 	}
 	return false
-}
-
-func getDataFibre(ps string) []string {
-	for _, data := range TFibre {
-		if ps == data.FoCode {
-
-			var cb string
-			for _, cbs := range TCable {
-				if cbs.CbCode == data.FoCbCode {
-					cb = cbs.CbEti
-					break
-				}
-			}
-
-			return []string{data.FoNumTube, data.FoColor, cb}
-		}
-	}
-	return []string{"", "", ""}
-}
-func getDataCassette(cs string) []string {
-	for _, data := range TCassette {
-		if cs == data.CsCode {
-			return []string{data.CsNum, data.CsBpCode}
-		}
-	}
-	return []string{"", ""}
-}
-func getDataEbp(bp string) string {
-	for _, data := range TEbp {
-		if bp == data.BpCode {
-			return data.BpEti
-		}
-	}
-	return ""
-}
-func getDataTirroir(ti string) string {
-	for _, data := range TTirroir {
-		if ti == data.TiCode {
-			return data.TiEti
-		}
-	}
-	return ""
 }
 
 func (d *Data) getFolderDLG() string {
@@ -425,6 +352,7 @@ func (d *Data) getFolderDLG() string {
 
 	return dlg
 }
+
 func (d *Data) getDLGErr() string {
 	var dlg string
 
@@ -441,4 +369,51 @@ func (d *Data) getDLGErr() string {
 	}
 
 	return dlg
+}
+
+//...
+// GetData
+func getDataFibre(ps string) []string {
+	for _, data := range TFibre {
+		if ps == data.FoCode {
+
+			var cb string
+			for _, cbs := range TCable {
+				if cbs.CbCode == data.FoCbCode {
+					cb = cbs.CbEti
+					break
+				}
+			}
+
+			return []string{data.FoNumTube, data.FoColor, cb}
+		}
+	}
+	return []string{"", "", ""}
+}
+
+func getDataCassette(cs string) []string {
+	for _, data := range TCassette {
+		if cs == data.CsCode {
+			return []string{data.CsNum, data.CsBpCode}
+		}
+	}
+	return []string{"", ""}
+}
+
+func getDataEbp(bp string) string {
+	for _, data := range TEbp {
+		if bp == data.BpCode {
+			return data.BpEti
+		}
+	}
+	return ""
+}
+
+func getDataTirroir(ti string) string {
+	for _, data := range TTirroir {
+		if ti == data.TiCode {
+			return data.TiEti
+		}
+	}
+	return ""
 }
